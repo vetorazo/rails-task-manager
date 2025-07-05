43ca22b7bb1bdec992aa7ff4e4877a197bde348a
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-05 13:03:11 +0900

    Finished the creating part, starting the edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,10 @@ class TasksController < ApplicationController
   end
 
   def create
-
+    # get attributes from parameter
+    task_params = params.require(:task).permit(:title, :details)
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
   end
 end
