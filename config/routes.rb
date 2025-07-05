e3e71ebbac2bd8cf44ec98340c2e5ec0f0b4c2b3
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-05 15:45:34 +0900

    Finished the app functionality and added some visuals with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  resources :tasks
 end
