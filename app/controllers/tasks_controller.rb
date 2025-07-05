519d72fd6188175ba05526bf7343311d9b5f21ad
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-05 14:56:23 +0900

    Added the edit part and the checkboxes to complete the task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,15 @@ class TasksController < ApplicationController
     @task.save
     redirect_to task_path(@task)
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    task_params = params.require(:task).permit(:title, :details)
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
 end
